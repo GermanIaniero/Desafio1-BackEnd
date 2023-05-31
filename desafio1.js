947328f79901a25fd5c3c65f89ec66961f696213
GermanIaniero <german_danielianiero@hotmail.com>
2023-05-31 13:23:45 -0300

    modificado 31/5/23 13:23

--- desafio1.js
+++ desafio1.js
@@ -2,43 +2,54 @@
 class ProductManager {
 
     constructor() {
-        this.products = []
+        this.products = [];
     }
 
-    getProducts = () => { return this.products}
-
-    getNextID = () => {
-        const count = this.products.length
- 
-        if (count > 0) {
-
-            return this.products[count - 1].id + 1
-        } else {
-            return 1
+    addProduct(title, description, price, thumbnail, code, stock) {
+        // Verificar que no se repita el código
+        const existingProduct = this.products.find((product) => product.code === code);
+        if (existingProduct) {
+          console.error("Error: el código ya existe");
+          return;
         }
-    }
 
-    addProduct = (title, description, price, thumbnail, code, stock) => {
-        const product = {
-            id: this.getNextID(),
-            
+
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.error("Error: faltan datos del producto");
+            return;
+        }
+        // Agregar el nuevo producto con un ID autoincrementable
+        const newProduct = {
+            id: this.products.length + 1,
             title,
             description,
             price,
             thumbnail,
             code,
-            stock
+            stock,
+        };
+        this.products.push(newProduct);
+        console.log("Producto agregado:", newProduct);
         }
 
-        this.products.push(product)
+    getProducts ()  { 
+        return this.products
     }
 
+    getProductById(id) {
+        const product = this.products.find((product) => product.id === id);
+        if (!product) {
+          console.error("Producto no encontrado");
+          return;
+        }
+        return product;
+      }
+    }
 
+    const manager = new ProductManager();
+  
+    manager.addProduct("producto prueba1", "Este es un producto prueba1", 200, "thumbnail1.jpg", "abc123", 25);
+    manager.addProduct("producto prueba2", "Este es un producto prueba2", 300, "thumbnail2.jpg", "abc124", 26);
+    console.log("Todos los productos:", manager.getProducts());
 
-}
-
-
-const newproducto = new ProductManager()
-newproducto.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25)
-newproducto.addProduct('producto prueba2', 'Este es un producto prueba2', 300, 'Sin imagen2', 'abc124', 26)
-console.log(newproducto.getProducts())
+    console.log("Producto con ID 2:", manager.getProductById(2));
